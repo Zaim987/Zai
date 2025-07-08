8420615c746c7af7d49fdd0d259ab9d94b746a70
Zaim987 <cantum23@gmail.com>
2025-07-08 15:15:00 +0700

    Add files via upload
    Signed-off-by: Zaim987 <cantum23@gmail.com>

---
+++ js/login.js
@@ -0,0 +1,16 @@
+
+import { auth } from './firebase-init.js';
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+document.getElementById("login-form")?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    window.location.href = "dashboard.html";
+  } catch (error) {
+    alert("Login gagal: " + error.message);
+  }
+});
